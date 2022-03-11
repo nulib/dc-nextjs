1aec30dd92f1bf594f824cea57565d1c817cd388
Mat Jordan <mat@northwestern.edu>
2022-03-11 14:11:10 -0500

    Add hierarchical organization.

--- lib/facets.ts
+++ lib/facets.ts
@@ -1,103 +1,115 @@
-const FACETS_CREATOR = [
-  {
-    componentId: "contributor",
-    field: "descriptiveMetadata.contributor.displayFacet",
-    title: "Contributor",
-  },
-  {
-    componentId: "creator",
-    field: "descriptiveMetadata.creator.displayFacet",
-    title: "Creator",
-  },
-];
+const FACETS_CREATOR = {
+  label: "Creator and Contributor",
+  facets: [
+    {
+      id: "contributor",
+      field: "descriptiveMetadata.contributor.displayFacet",
+      label: "Contributor",
+    },
+    {
+      id: "creator",
+      field: "descriptiveMetadata.creator.displayFacet",
+      label: "Creator",
+    },
+  ],
+};
 
-const FACETS_RIGHTS_USAGE = [
-  {
-    componentId: "rightsStatement",
-    field: "descriptiveMetadata.rightsStatement.label.keyword",
-    title: "Rights Statement",
-  },
-];
+const FACETS_RIGHTS_USAGE = {
+  label: "Rights and Usage",
+  facets: [
+    {
+      id: "rightsStatement",
+      field: "descriptiveMetadata.rightsStatement.label.keyword",
+      label: "Rights Statement",
+    },
+  ],
+};
 
-const FACETS_DESCRIPTIVE = [
-  {
-    componentId: "genre",
-    field: "descriptiveMetadata.genre.displayFacet",
-    title: "Genre",
-  },
-  {
-    componentId: "language",
-    field: "descriptiveMetadata.language.displayFacet",
-    title: "Language",
-  },
-  {
-    componentId: "Location",
-    field: "descriptiveMetadata.location.displayFacet",
-    title: "Location",
-  },
-  {
-    componentId: "stylePeriod",
-    field: "descriptiveMetadata.stylePeriod.displayFacet",
-    title: "Style Period",
-  },
-  {
-    componentId: "subject",
-    field: "descriptiveMetadata.subject.term.label.keyword",
-    title: "Subject",
-  },
-  {
-    componentId: "technique",
-    field: "descriptiveMetadata.technique.displayFacet",
-    title: "Technique",
-  },
-  {
-    componentId: "workType",
-    field: "workType.label.keyword",
-    title: "Work Type",
-  },
-];
+const FACETS_DESCRIPTIVE = {
+  label: "Subjects and Descriptive",
+  facets: [
+    {
+      id: "genre",
+      field: "descriptiveMetadata.genre.displayFacet",
+      label: "Genre",
+    },
+    {
+      id: "language",
+      field: "descriptiveMetadata.language.displayFacet",
+      label: "Language",
+    },
+    {
+      id: "Location",
+      field: "descriptiveMetadata.location.displayFacet",
+      label: "Location",
+    },
+    {
+      id: "stylePeriod",
+      field: "descriptiveMetadata.stylePeriod.displayFacet",
+      label: "Style Period",
+    },
+    {
+      id: "subject",
+      field: "descriptiveMetadata.subject.term.label.keyword",
+      label: "Subject",
+    },
+    {
+      id: "technique",
+      field: "descriptiveMetadata.technique.displayFacet",
+      label: "Technique",
+    },
+    {
+      id: "workType",
+      field: "workType.label.keyword",
+      label: "Work Type",
+    },
+  ],
+};
 
-const FACETS_LOCATION = [
-  {
-    componentId: "libraryDepartment",
-    field: "administrativeMetadata.libraryUnit.label.keyword",
-    title: "Library Department",
-  },
-  {
-    componentId: "collection",
-    field: "collection.title.keyword",
-    title: "Collection",
-  },
-  {
-    componentId: "boxName",
-    field: "descriptiveMetadata.boxName.keyword",
-    title: "Box Name",
-  },
-  {
-    componentId: "boxNumber",
-    field: "descriptiveMetadata.boxNumber.keyword",
-    title: "Box Number",
-  },
-  {
-    componentId: "folderName",
-    field: "descriptiveMetadata.folderName.keyword",
-    title: "Folder Name",
-  },
-  {
-    componentId: "folderNumber",
-    field: "descriptiveMetadata.folderNumber.keyword",
-    title: "Folder Number",
-  },
-  {
-    componentId: "series",
-    field: "descriptiveMetadata.series.keyword",
-    title: "Series",
-  },
-];
+const FACETS_LOCATION = {
+  label: "Location",
+  facets: [
+    {
+      id: "libraryDepartment",
+      field: "administrativeMetadata.libraryUnit.label.keyword",
+      label: "Library Department",
+    },
+    {
+      id: "collection",
+      field: "collection.title.keyword",
+      label: "Collection",
+    },
+    {
+      id: "boxName",
+      field: "descriptiveMetadata.boxName.keyword",
+      label: "Box Name",
+    },
+    {
+      id: "boxNumber",
+      field: "descriptiveMetadata.boxNumber.keyword",
+      label: "Box Number",
+    },
+    {
+      id: "folderName",
+      field: "descriptiveMetadata.folderName.keyword",
+      label: "Folder Name",
+    },
+    {
+      id: "folderNumber",
+      field: "descriptiveMetadata.folderNumber.keyword",
+      label: "Folder Number",
+    },
+    {
+      id: "series",
+      field: "descriptiveMetadata.series.keyword",
+      label: "Series",
+    },
+  ],
+};
 
 export const FACETS = [
-  ...FACETS_CREATOR,
-  ...FACETS_DESCRIPTIVE,
-  ...FACETS_LOCATION,
-  ...FACETS_RIGHTS_USAGE,
+  FACETS_CREATOR,
+  FACETS_DESCRIPTIVE,
+  FACETS_LOCATION,
+  FACETS_RIGHTS_USAGE,
 ];
