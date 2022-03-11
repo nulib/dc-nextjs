3a3fc6a53d22e9e142cfeddf02072e0e98627e36
Mat Jordan <mat@northwestern.edu>
2022-03-11 16:48:27 -0500

    Add search constants to build aggs. (#5)
    * Add facet consts.
    
    * Add hierarchical organization.
    
    * Add types.
    
    * Add programatic build of aggs from constants.

---
+++ lib/facets.ts
@@ -0,0 +1,125 @@
+interface FacetsInstance {
+  id: string;
+  field: string;
+  label: string;
+}
+export interface FacetsGroup {
+  label: string;
+  facets: FacetsInstance[];
+}
+
+const FACETS_CREATOR: FacetsGroup = {
+  label: "Creator and Contributor",
+  facets: [
+    {
+      id: "contributor",
+      field: "descriptiveMetadata.contributor.displayFacet",
+      label: "Contributor",
+    },
+    {
+      id: "creator",
+      field: "descriptiveMetadata.creator.displayFacet",
+      label: "Creator",
+    },
+  ],
+};
+
+const FACETS_RIGHTS_USAGE: FacetsGroup = {
+  label: "Rights and Usage",
+  facets: [
+    {
+      id: "rightsStatement",
+      field: "descriptiveMetadata.rightsStatement.label.keyword",
+      label: "Rights Statement",
+    },
+  ],
+};
+
+const FACETS_DESCRIPTIVE: FacetsGroup = {
+  label: "Subjects and Descriptive",
+  facets: [
+    {
+      id: "genre",
+      field: "descriptiveMetadata.genre.displayFacet",
+      label: "Genre",
+    },
+    {
+      id: "language",
+      field: "descriptiveMetadata.language.displayFacet",
+      label: "Language",
+    },
+    {
+      id: "location",
+      field: "descriptiveMetadata.location.displayFacet",
+      label: "Location",
+    },
+    {
+      id: "stylePeriod",
+      field: "descriptiveMetadata.stylePeriod.displayFacet",
+      label: "Style Period",
+    },
+    {
+      id: "subject",
+      field: "descriptiveMetadata.subject.term.label.keyword",
+      label: "Subject",
+    },
+    {
+      id: "technique",
+      field: "descriptiveMetadata.technique.displayFacet",
+      label: "Technique",
+    },
+    {
+      id: "workType",
+      field: "workType.label.keyword",
+      label: "Work Type",
+    },
+  ],
+};
+
+const FACETS_LOCATION: FacetsGroup = {
+  label: "Location",
+  facets: [
+    {
+      id: "libraryDepartment",
+      field: "administrativeMetadata.libraryUnit.label.keyword",
+      label: "Library Department",
+    },
+    {
+      id: "collection",
+      field: "collection.title.keyword",
+      label: "Collection",
+    },
+    {
+      id: "boxName",
+      field: "descriptiveMetadata.boxName.keyword",
+      label: "Box Name",
+    },
+    {
+      id: "boxNumber",
+      field: "descriptiveMetadata.boxNumber.keyword",
+      label: "Box Number",
+    },
+    {
+      id: "folderName",
+      field: "descriptiveMetadata.folderName.keyword",
+      label: "Folder Name",
+    },
+    {
+      id: "folderNumber",
+      field: "descriptiveMetadata.folderNumber.keyword",
+      label: "Folder Number",
+    },
+    {
+      id: "series",
+      field: "descriptiveMetadata.series.keyword",
+      label: "Series",
+    },
+  ],
+};
+
+export const FACETS: FacetsGroup[] = [
+  FACETS_CREATOR,
+  FACETS_DESCRIPTIVE,
+  FACETS_LOCATION,
+  FACETS_RIGHTS_USAGE,
+];
--- lib/queries/aggs.ts
+++ lib/queries/aggs.ts
@@ -1,31 +1,25 @@
+import { FACETS, FacetsGroup } from "lib/facets";
+
+export const buildAggs = (facets: FacetsGroup[]) => {
+  const aggs: any = {};
+
+  facets.forEach((groups) => {
+    groups.facets.forEach((facet) => {
+      let terms = Object.create({});
+      terms.field = facet.field;
+      terms.size = 10;
+      terms.order = {
+        _count: "desc",
+      };
+
+      aggs[facet.id] = Object.create({});
+      aggs[facet.id].terms = terms;
+    });
+  });
+
+  return aggs;
+};
+
 export const aggs = {
-  aggs: {
-    contributor: {
-      terms: {
-        field: "descriptiveMetadata.contributor.displayFacet",
-        size: 10,
-        order: {
-          _count: "desc",
-        },
-      },
-    },
-    genre: {
-      terms: {
-        field: "descriptiveMetadata.genre.displayFacet",
-        size: 10,
-        order: {
-          _count: "desc",
-        },
-      },
-    },
-    subject: {
-      terms: {
-        field: "descriptiveMetadata.subject.displayFacet",
-        size: 10,
-        order: {
-          _count: "desc",
-        },
-      },
-    },
-  },
+  aggs: buildAggs(FACETS),
 };
