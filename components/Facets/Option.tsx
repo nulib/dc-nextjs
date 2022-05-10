2ef4e972675c475c39d348b180c6bbf203f902f4
Mat Jordan <mat@northwestern.edu>
2022-05-10 15:57:45 -0400

    Refine handleCheckedChange logic on facet option.

--- components/Facets/Option.tsx
+++ components/Facets/Option.tsx
@@ -1,6 +1,7 @@
 import * as Checkbox from "@radix-ui/react-checkbox";
 import React, { useEffect, useState } from "react";
 import { FacetOption } from "@/types/components/facets";
+import { UserFacets } from "@/types";
 import { useFilterState } from "@/context/filter-context";
 import { useSearchState } from "@/context/search-context";
 
@@ -9,19 +10,36 @@ const Option: React.FC<FacetOption> = ({ bucket, facet, index, type }) => {
   const id = `${facet}-${index}`;
 
   const { searchState } = useSearchState();
-  const { filterDispatch } = useFilterState();
+  const { filterDispatch, filterState } = useFilterState();
   const [checked, setChecked] = useState<boolean>(false);
 
-  useEffect(() => {
-    const { userFacets } = searchState;
-    if (userFacets[facet])
-      if (userFacets[facet].includes(key)) setChecked(true);
-  }, [searchState, facet, key]);
+  const { userFacetsUnsubmitted } = filterState;
+
+  // useEffect(() => {
+  //   const { userFacets } = searchState;
+  //   if (userFacets[facet])
+  //     if (userFacets[facet].includes(key)) setChecked(true);
+  // }, [searchState, facet, key]);
 
   const handleCheckedChange = (checkedStatus: boolean) => {
     setChecked(checkedStatus);
+    const newObj: UserFacets = { ...userFacetsUnsubmitted };
+
+    // Checkbox is checked
+    if (checkedStatus) {
+      if (!newObj[facet]) {
+        newObj[facet] = [key];
+      } else {
+        newObj[facet].push(key);
+      }
+    }
+    // Not checked, remove value from the array
+    else {
+      newObj[facet] = [...newObj[facet]].filter((arrValue) => arrValue !== key);
+    }
+
     filterDispatch({
-      type: checkedStatus ? "addUserFacet" : "removeUserFacet",
+      type: "updateUserFacets",
       userFacetsUnsubmitted: { [facet]: [key] },
     });
   };
