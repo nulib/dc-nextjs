835f02fa66669a86f5dcac2dead8d5dcc5159803
Mat Jordan <mat@northwestern.edu>
2022-05-16 13:21:32 -0400

    Remove fragment wrapper.

--- components/Facets/Filter.tsx
+++ components/Facets/Filter.tsx
@@ -32,19 +32,17 @@ const FilterModal: React.FC = () => {
 
 const FacetsFilter: React.FC = () => {
   return (
-    <>
-      <Dialog.Root>
-        <FilterTrigger>
-          <span>Filter</span>
-        </FilterTrigger>
-        <Dialog.Portal>
-          <FilterOverlay />
-          <FilterContent>
-            <FilterModal />
-          </FilterContent>
-        </Dialog.Portal>
-      </Dialog.Root>
-    </>
+    <Dialog.Root>
+      <FilterTrigger>
+        <span>Filter</span>
+      </FilterTrigger>
+      <Dialog.Portal>
+        <FilterOverlay />
+        <FilterContent>
+          <FilterModal />
+        </FilterContent>
+      </Dialog.Portal>
+    </Dialog.Root>
   );
 };
 
