41b8fdffccfa76b37deedc5da2aa279d2813b780
Karen Shaw <karendid@gmail.com>
2022-07-15 11:06:23 -0600

    Add env variables in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  env: {
+    JWT_TOKEN_SECRET: process.env.JWT_TOKEN_SECRET,
+    NUSSO_API_KEY: process.env.NUSSO_API_KEY,
+    NUSSO_BASE_URL: process.env.NUSSO_BASE_URL,
+  },
   reactStrictMode: true,
-}
+};
