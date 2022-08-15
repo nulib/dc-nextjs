f809b42f0b5a91ce00306ddd626711e881edb5a2
Mat Jordan <mat@northwestern.edu>
2022-08-15 11:49:28 -0400

    Add prop drilling for header styling on layout.

---
+++ components/Home/Overview.tsx
@@ -0,0 +1,7 @@
+import Container from "@/components/Shared/Container";
+
+const Overview = () => {
+  return <Container>blah</Container>;
+};
+
+export default Overview;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
-import Header from "@/components/Header/Header";
+import Layout from "@/components/layout";
+import Overview from "@/components/Home/Overview";
 import { PlaceholderBlock } from "@/components/Shared/PlaceholderBlock.styled";
 
 const HomePage: React.FC = () => {
   return (
-    <div data-testid="home-page-wrapper">
-      <Header isHero />
-      <PlaceholderBlock css={{ height: "200vh" }}>...</PlaceholderBlock>
-    </div>
+    <Layout header="hero">
+      <Overview />
+      <PlaceholderBlock css={{ height: "100vh" }} />
+    </Layout>
   );
 };
 
