47d2ab5c0d9cc41e2c0c5d9aba465d40871d1a33
Karen Shaw <karendid@gmail.com>
2022-10-07 21:12:23 +0000

    Use DC API authentication routes

--- lib/constants/auth.ts
+++ lib/constants/auth.ts
@@ -1,4 +1,4 @@
-export const API_TOKEN_COOKIE = "dcApiToken_v2";
+export const API_TOKEN_COOKIE = "dcApiV2Token";
 export const AUTH_DOMAIN = ".library.northwestern.edu";
 export const NUSSO_COOKIE = "nusso";
 export const NUSSO_REDIRECT_URL = "nussoRedirectUrl";
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,12 @@ import { API_TOKEN_COOKIE, AUTH_DOMAIN } from "@/lib/constants/auth";
 import { User, UserContextInterface } from "@/types/context/user";
 import { deleteCookie, getCookie } from "cookies-next";
 import type { AppProps } from "next/app";
+import { DCAPI_ENDPOINT } from "@/lib/constants/endpoints";
 import React from "react";
 import { SearchProvider } from "@/context/search-context";
 import Transition from "@/components/Transition";
 import axios from "axios";
 import globalStyles from "@/styles/global";
-
 
 export const UserContext = React.createContext<UserContextInterface | null>(
   null
@@ -24,15 +24,19 @@ function MyApp({ Component, pageProps }: AppProps) {
      */
     const token = getCookie(API_TOKEN_COOKIE);
     if (token) {
-      axios.get("/api/auth/whoami").then((result) => {
-        if (!result.data) return;
+      axios
+        .get(`${DCAPI_ENDPOINT}/auth/whoami`, {
+          withCredentials: true,
+        })
+        .then((result) => {
+          if (!result.data) return;
 
-        const { displayName, mail } = result.data;
-        setUser({
-          displayName: displayName[0],
-          mail,
+          const { displayName, mail } = result.data;
+          setUser({
+            displayName: displayName[0],
+            mail,
+          });
         });
-      });
     }
   }, []);
 
