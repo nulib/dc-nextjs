c49f9eb9936a58f207204838c54cf29769c57001
Mat Jordan <mat@northwestern.edu>
2023-01-13 10:46:20 -0500

    Address footer styling.

--- components/Footer/Footer.styled.ts
+++ components/Footer/Footer.styled.ts
@@ -1,5 +1,12 @@
 import { styled } from "@/stitches.config";
 
+/* eslint sort-keys: 0 */
+
 export const FooterStyled = styled("div", {
-  background: "$black80",
+  background: "$purple",
+  padding: "$gr2 0",
+
+  "a, a:visited": {
+    color: "$white !important",
+  },
 });
