2804a708c76694f31c21da9987dee30949e136d9
Adam J. Arling <aarling@gmail.com>
2023-02-09 15:19:37 -0600

    Reconfigure Honeybadger to upload and make use of source maps
    Co-Authored-By: Michael B. Klein <mbklein@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,14 +1,27 @@
+const fs = require("fs");
 const HoneybadgerSourceMapPlugin = require("@honeybadger-io/webpack");
 
 // Use the HoneybadgerSourceMapPlugin to upload the source maps during build step
 const {
   HONEYBADGER_API_KEY,
   HONEYBADGER_ENV,
-  HONEYBADGER_ASSETS_URL,
   HONEYBADGER_REPORT_DATA,
+  NEXT_PUBLIC_DC_URL,
 } = process.env;
 const NODE_ENV = process.env.HONEYBADGER_ENV || process.env.NODE_ENV;
-const HONEYBADGER_REVISION = process.env.AWS_COMMIT_ID;
+const HONEYBADGER_REVISION = process.env.HONEYBADGER_REVISION || process.env.AWS_COMMIT_ID;
+
+const HoneybadgerConfig = JSON.stringify({
+  HONEYBADGER_API_KEY,
+  HONEYBADGER_ENV,
+  HONEYBADGER_REPORT_DATA,
+  HONEYBADGER_REVISION,
+}, null, 2);
+
+fs.writeFileSync(
+  "lib/honeybadger/config.vars.js",
+  `module.exports = ${HoneybadgerConfig};`
+);
 
 /** @type {import('next').NextConfig} */
 module.exports = {
@@ -28,7 +41,7 @@ module.exports = {
     ],
   },
   reactStrictMode: true,
-  swcMinify: false,
+  swcMinify: true,
   webpack: (config) => {
     // When all the Honeybadger configuration env variables are
     // available/configured The Honeybadger webpack plugin gets pushed to the
@@ -36,10 +49,10 @@ module.exports = {
     // This is an alternative to manually uploading the source maps.
     // See https://docs.honeybadger.io/lib/javascript/guides/using-source-maps.html
     // Note: This is disabled in development mode.
+
     if (
       HONEYBADGER_API_KEY &&
-      HONEYBADGER_ASSETS_URL &&
-      NODE_ENV === "production"
+      (NODE_ENV === "production" || NODE_ENV === "staging")
     ) {
       // `config.devtool` must be 'hidden-source-map' or 'source-map' to properly pass sourcemaps.
       // Next.js uses regular `source-map` which doesnt pass its sourcemaps to Webpack.
@@ -47,11 +60,10 @@ module.exports = {
       // Use the hidden-source-map option when you don't want the source maps to be
       // publicly available on the servers, only to the error reporting
       config.devtool = "hidden-source-map";
-
       config.plugins.push(
         new HoneybadgerSourceMapPlugin({
           apiKey: HONEYBADGER_API_KEY,
-          assetsUrl: HONEYBADGER_ASSETS_URL,
+          assetsUrl: `${NEXT_PUBLIC_DC_URL}/_next`,
           revision: HONEYBADGER_REVISION,
         })
       );
