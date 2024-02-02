850bca72afeebccc48f7809c6f8d6dbe8e97e6d2
Mat Jordan <mat@northwestern.edu>
2024-02-02 14:04:21 -0500

    Shift Chat to search screen, rename directory.

--- pages/search.tsx
+++ pages/search.tsx
@@ -7,6 +7,7 @@ import React, { useEffect, useState } from "react";
 
 import { ApiSearchRequestBody } from "@/types/api/request";
 import { ApiSearchResponse } from "@/types/api/response";
+import ChatWrapper from "@/components/Chat/components/Wrapper";
 import Container from "@/components/Shared/Container";
 import { DC_API_SEARCH_URL } from "@/lib/constants/endpoints";
 import Facets from "@/components/Facets/Facets";
@@ -176,6 +177,8 @@ const SearchPage: NextPage = () => {
             work={similarTo.work}
           />
         )}
+
+        <ChatWrapper />
 
         <Facets />
 
