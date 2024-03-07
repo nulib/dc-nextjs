8f563f22847c7177d08766ff6208385b364cb50f
Adam Joseph Arling <aarling@gmail.com>
2024-03-07 15:14:55 -0600

    Update chat API endpoint to use ENV value like other endpoints.  Based on backend work to support this.

--- hooks/useChatSocket.ts
+++ hooks/useChatSocket.ts
@@ -3,9 +3,6 @@ import { useEffect, useState } from "react";
 import { DCAPI_CHAT_ENDPOINT } from "@/lib/constants/endpoints";
 import axios from "axios";
 
-// URL which the browser seems to have to hit to authenticate with the chat endpoint
-// https://api.dc.library.northwestern.edu/api/v2/auth/login?goto=https://api.dc.library.northwestern.edu/api/v2/chat-endpoint
-
 const useChatSocket = () => {
   const [chatSocket, setChatSocket] = useState<WebSocket | null>(null);
   const [authToken, setAuthToken] = useState<string | null>(null);
