64c6112c25f4b0c70dcbb91349eb977e33cf2b61
Adam Joseph Arling <aarling@gmail.com>
2024-05-16 08:33:21 -0500

    Prompt user login when ai chat query param is in the url and user is not logged in; mainly targets new page views or link sharing

--- hooks/useGenerativeAISearchToggle.ts
+++ hooks/useGenerativeAISearchToggle.ts
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import { DCAPI_ENDPOINT } from "@/lib/constants/endpoints";
 import { UserContext } from "@/context/user-context";
@@ -13,13 +13,22 @@ const aiQueryParam = "ai";
 
 export default function useGenerativeAISearchToggle() {
   const router = useRouter();
-  const isChecked = router.query[aiQueryParam] === "true";
+  const [dialog, setDialog] = useState(defaultModalState);
 
   const { user } = React.useContext(UserContext);
 
-  const [dialog, setDialog] = useState(defaultModalState);
+  const isAiQueryParam = router.query[aiQueryParam] === "true";
+  const isChecked = isAiQueryParam && user?.isLoggedIn;
 
   const loginUrl = `${DCAPI_ENDPOINT}/auth/login?goto=${goToLocation()}`;
+
+  // If the "ai" query param is present and the user is not logged in on
+  // initial load, show the dialog
+  useEffect(() => {
+    if (isAiQueryParam && !user?.isLoggedIn) {
+      setDialog((prevDialog) => ({ ...prevDialog, isOpen: true }));
+    }
+  }, [isAiQueryParam, user?.isLoggedIn]);
 
   function goToLocation() {
     const currentUrl = `${window.location.origin}${router.asPath}`;
