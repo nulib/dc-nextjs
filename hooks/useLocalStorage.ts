c3004768ae468b50b0931b9ac4ca609717f94d46
Mat Jordan <mat@northwestern.edu>
2024-07-26 12:17:08 -0400

    Check for typeof window on useLocalStorage hook.

--- hooks/useLocalStorage.ts
+++ hooks/useLocalStorage.ts
@@ -3,32 +3,36 @@ import { useCallback, useEffect, useState } from "react";
 function useLocalStorage(key: string, initialValue: string) {
   // Get the initial value from localStorage or use the provided initialValue
   const [storedValue, setStoredValue] = useState(() => {
-    try {
-      const item = window.localStorage.getItem(key);
-      if (item) {
-        return JSON.parse(item);
-      } else {
-        // If no item exists, set the initial value in localStorage
-        window.localStorage.setItem(key, JSON.stringify(initialValue));
+    if (typeof window !== "undefined") {
+      try {
+        const item = window.localStorage.getItem(key);
+        if (item) {
+          return JSON.parse(item);
+        } else {
+          // If no item exists, set the initial value in localStorage
+          window.localStorage.setItem(key, JSON.stringify(initialValue));
+          return initialValue;
+        }
+      } catch (error) {
+        console.error(error);
         return initialValue;
       }
-    } catch (error) {
-      console.error(error);
-      return initialValue;
     }
   });
 
   // Function to update localStorage and notify other components
   const setValue = useCallback(
     (value: any) => {
-      try {
-        const valueToStore =
-          value instanceof Function ? value(storedValue) : value;
-        setStoredValue(valueToStore);
-        window.localStorage.setItem(key, JSON.stringify(valueToStore));
-        window.dispatchEvent(new Event("local-storage"));
-      } catch (error) {
-        console.error(error);
+      if (typeof window !== "undefined") {
+        try {
+          const valueToStore =
+            value instanceof Function ? value(storedValue) : value;
+          setStoredValue(valueToStore);
+          window.localStorage.setItem(key, JSON.stringify(valueToStore));
+          window.dispatchEvent(new Event("local-storage"));
+        } catch (error) {
+          console.error(error);
+        }
       }
     },
     [key, storedValue],
@@ -37,13 +41,15 @@ function useLocalStorage(key: string, initialValue: string) {
   // Listen for changes in other components
   useEffect(() => {
     const handleStorageChange = () => {
-      try {
-        const item = window.localStorage.getItem(key);
-        if (item) {
-          setStoredValue(JSON.parse(item));
+      if (typeof window !== "undefined") {
+        try {
+          const item = window.localStorage.getItem(key);
+          if (item) {
+            setStoredValue(JSON.parse(item));
+          }
+        } catch (error) {
+          console.error(error);
         }
-      } catch (error) {
-        console.error(error);
       }
     };
 
