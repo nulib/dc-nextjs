4e4b394376bd012cfe034434777dedf1d59f1c93
Brendan Quinn <brendan-quinn@northwestern.edu>
2025-10-31 14:27:22 +0000

    Move disclaimer to footer content

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -2,7 +2,6 @@ import Container from "@/components/Shared/Container";
 import { FooterContent } from "@/components/Footer/Content";
 import { FooterStyled } from "@/components/Footer/Footer.styled";
 import React from "react";
-import SiteContentMessage from "@/components/Footer/SiteContentMessage/SiteContentMessage";
 
 export default function Footer() {
   return (
@@ -10,7 +9,6 @@ export default function Footer() {
       <Container data-testid="footer">
         <FooterContent />
       </Container>
-      <SiteContentMessage />
     </FooterStyled>
   );
 }
